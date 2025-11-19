6c3ddbf7192d9e20628f0663b63c33f43e836f3a
Donnersten <hampus@donnersten.se>
2025-11-19 15:43:05 +0100

    hemkop and willys size

--- willys/willys_url.py
+++ willys/willys_url.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 
-get_response = requests.get('https://www.willys.se/search/campaigns/online?q=2110&type=PERSONAL_GENERAL&page=0&size=20')
+get_response = requests.get('https://www.willys.se/search/campaigns/online?q=2110&type=PERSONAL_GENERAL&page=0&size=1000')
 
 data = get_response.json()  # Konvertera text till Python-objekt
 
